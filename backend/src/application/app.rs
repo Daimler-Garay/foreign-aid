0432c894bc46de38876680393973c9f975320d10
Daimler Garay <daimler.garay@priceindustries.com>
2026-05-29 07:57:10 -0500

    Wire up the applicatio

--- backend/src/application/app.rs
+++ backend/src/application/app.rs
@@ -1,3 +1,27 @@
 use std::sync::Arc;
 
 use tokio::sync::Mutex;
+
+use crate::{
+    api::server,
+    application::{config, state::AppState},
+    db::Database,
+};
+
+pub async fn run() {
+    let config = config::load();
+
+    let db_pool = Database::connect(config.clone().into())
+        .await
+        .expect("Failed to connect to the database");
+
+    // execute migrations
+    Database::migrate(&db_pool)
+        .await
+        .expect("Failed to run database migrations");
+
+    // build app state
+    let shared_state = Arc::new(AppState { config, db_pool });
+
+    server::start(shared_state).await;
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,6 @@
-use axum::{Router, routing::get};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use crate::application::app;
 
 mod api;
 mod application;
@@ -23,17 +24,5 @@ async fn main() {
 
     tracing::info!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
 
-    // build our application with a route
-    let app = Router::new().route("/health", get(health));
-
-    // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-    let _ = axum::serve(listener, app).await;
-}
-
-async fn health() -> &'static str {
-    "Ok"
+    app::run().await;
 }
